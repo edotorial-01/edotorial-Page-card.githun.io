5059c578ed3948d96fdfb38938c8fb38f4812815
edotorial-01 <151850452+edotorial-01@users.noreply.github.com>
2023-11-30 17:15:25 -0800

    Add files via upload

---
+++ coba.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
